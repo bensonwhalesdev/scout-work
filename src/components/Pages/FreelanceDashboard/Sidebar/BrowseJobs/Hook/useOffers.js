b959ad35088081e9a11c2bdfbb38112abbf054c7
bensonwhalesdev <lawrencebenson18@hotmail.com>
2025-07-05 15:50:40 -0700

    files updated

--- src/components/Pages/FreelanceDashboard/Sidebar/BrowseJobs/Hook/useOffers.js
+++ src/components/Pages/FreelanceDashboard/Sidebar/BrowseJobs/Hook/useOffers.js
@@ -4,37 +4,36 @@ import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 const useOffers = () => {
-    const { user } = useGetUserStore();
-      const [offers, setOffers] = useState([]);
-    
-      useEffect(() => {
-        const fetchOffers = async () => {
-          try {
-            const res = await apiClient.get(`/joboffer/freelancer/${user._id}`);
-            setOffers(res.data);
-          } catch (err) {
-            toast.error("Failed to fetch offers");
-          }
-        };
-    
-        if (user) fetchOffers();
-      }, [user]);
-    
-      const handleAction = async (offerId, status) => {
-        try {
-          await apiClient.patch(`/joboffer/${offerId}`, { status });
-          toast.success(`Offer ${status}`);
-          setOffers((prev) =>
-            prev.map((offer) =>
-              offer._id === offerId ? { ...offer, status } : offer
-            )
-          );
-        } catch {
-          toast.error("Failed to update offer");
-        }
-      };
+  const { user } = useGetUserStore();
+  const [offers, setOffers] = useState([]);
 
-      return { offers, handleAction };
-}
+  useEffect(() => {
+    const fetchOffers = async () => {
+      try {
+        const res = await apiClient.get(`/joboffer/freelancer/${user._id}`);
+
+        const pendingOffers = res.data.filter(offer => offer.status === "pending");
+        setOffers(pendingOffers);
+      } catch (err) {
+        toast.error("Failed to fetch offers");
+      }
+    };
+
+    if (user) fetchOffers();
+  }, [user]);
+
+  const handleAction = async (offerId, status) => {
+    try {
+      await apiClient.patch(`/joboffer/${offerId}`, { status });
+      toast.success(`Offer ${status}`);
+      
+      setOffers((prev) => prev.filter((offer) => offer._id !== offerId));
+    } catch {
+      toast.error("Failed to update offer");
+    }
+  };
+
+  return { offers, handleAction };
+};
 
 export default useOffers;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
+    <Toaster richColors position="top-center" />
     <RouterProvider router={App} />
   </StrictMode>,
 )
