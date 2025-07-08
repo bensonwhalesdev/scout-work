fb1fe7edf93970623694ad7641e337fc74424ea4
bensonwhalesdev <lawrencebenson18@hotmail.com>
2025-07-07 18:08:34 -0700

    files updated

--- src/Hook/useLogout.js
+++ src/Hook/useLogout.js
@@ -1,3 +1,25 @@
+// import { toast } from "sonner";
+// import { useNavigate } from "react-router-dom";
+import { apiClient } from "@/lib/client";
+
+// const useLogout = () => {
+//   const navigate = useNavigate();
+
+//   const logout = async () => {
+//     try {
+//       await apiClient.post("/auth/logout", {}, { withCredentials: true });
+//       toast.success("Logged out successfully.");
+//       navigate("/");
+//     } catch (err) {
+//       toast.error(err.response?.data?.message || "Logout failed.");
+//     }
+//   };
+
+//   return logout;
+// };
+
+// export default useLogout;
+
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import Cookies from "js-cookie";
@@ -7,14 +29,20 @@ const useLogout = () => {
   const navigate = useNavigate();
   const { clearUser } = useGetUserStore();
 
-  const logout = () => {
-    clearUser();
-    Cookies.remove("token");
-    navigate("/", { replace: true });
-    toast.success("Logged out successfully");
+  const logout = async () => {
+    try {
+      clearUser();
+      await apiClient.post("/auth/logout", {}, { withCredentials: true });
+      Cookies.remove("token");
+      navigate("/", { replace: true });
+      toast.success("Logged out successfully");
+    } catch (error) {
+       toast.error(err.response?.data?.message || "Logout failed.");
+    }
   };
 
   return logout;
 };
 
 export default useLogout;
+
